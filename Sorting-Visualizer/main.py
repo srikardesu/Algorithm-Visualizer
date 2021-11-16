a7538ff8b594cc23dbc3190eed07e89e2ce4e298
Srikar Desu <srikarbhavesh03@gmail.com>
2021-11-17 01:29:05 +0530

    done with bubble sort visualisation

--- Sorting-Visualizer/main.py
+++ Sorting-Visualizer/main.py
@@ -18,17 +18,52 @@ from sorting_algorithms.bubbleSort import bubblesort
 # bubblesort(lst)
 # print(lst)
 
+def draw(data, color):
+    canvas.delete("all")
+    canvas_width = 1800
+    canvas_height = 1200
+    x_width = canvas_width / (len(data) + 1)
+    offset = 4
+    spacing = 2
+    normalizedData = [i / max(data) for i in data]
+
+    for i, height in enumerate(normalizedData):
+        x0 = i * x_width + offset + spacing
+        y0 = canvas_height - height * 600
+        x1 = (i + 1) * x_width + offset
+        y1 = canvas_height
+        canvas.create_rectangle(x0, y0, x1, y1, fill=color[i])
+
+    window.update_idletasks()
+
 def generate():
     global data
     data = []
     for i in range(0, 100):
         random_value = random.randint(1, 300)
         data.append(random_value)
+    #got the random array, time to plot it now
     print(data)
+    color = [LIGHT_GREEN for x in range(0,100)]
+    draw(data,color)
+
+def speed():
+    if speed_menu.get() == 'Slow':
+        return 0.05
+    elif speed_menu.get() == 'Medium':
+        return 0.01
+    else:
+        return 0.0005
+
+def sort():
+    global data
+    Tick = speed()
+    if algo_menu.get() == 'Bubble Sort':
+        bubblesort(data, draw, Tick)
 
 window = Tk()
 window.title("Sorting Algorithms Visualization")
-window.maxsize(1000, 700)
+window.maxsize(2400, 1600)
 window.config(bg=WHITE)
 
 algorithm_name = StringVar()
@@ -52,10 +87,10 @@ speed_menu = ttk.Combobox(UI_frame, textvariable=speed_name, values=speed_list)
 speed_menu.grid(row=1, column=1, padx=5, pady=5)
 speed_menu.current(0)
 
-canvas = Canvas(window, width=800, height=400, bg=WHITE)
+canvas = Canvas(window, width=1800, height=1200, bg=WHITE)
 canvas.grid(row=1, column=0, padx=10, pady=5)
 
-b1 = Button(UI_frame, text="Sort", command=bubblesort, bg=LIGHT_GRAY)
+b1 = Button(UI_frame, text="Sort", command=sort, bg=LIGHT_GRAY)
 b1.grid(row=2, column=1, padx=5, pady=5)
 
 b3 = Button(UI_frame, text="Generate Array", command=generate, bg=LIGHT_GRAY)
--- Sorting-Visualizer/sorting_algorithms/bubbleSort.py
+++ Sorting-Visualizer/sorting_algorithms/bubbleSort.py
@@ -1,10 +1,15 @@
 import time
 from colors import *
 
-def bubblesort(data):
-    size = len(data)
-    for i in range(size-1):
-        for j in range(size-i-1):
+def bubblesort(data,draw,Tick):
+    for i in range(99):
+        for j in range(99-i):
             if data[j] > data[j+1]:
                 data[j], data[j+1] = data[j+1], data[j]
+                colorstore = [YELLOW if x == j or x == j+1 else RED for x in range(0,100)]
+                draw(data, colorstore)
+                time.sleep(Tick)
+    
+    color = [LIGHT_GREEN for x in range(0,100)]
+    draw(data,color)
     
